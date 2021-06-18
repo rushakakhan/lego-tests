d178f20749119b993ac6318b57122250c7e39588
Craig Rushon <craig.rushon@shopify.com>
2021-06-18 12:15:38 -0400

    More tests added

--- lego_test.rb
+++ lego_test.rb
@@ -7,6 +7,26 @@ class TestLego < Minitest::Test
     @lego_hat = LegoHat.new("medium", "blue", "ball cap")
     @lego_jim = LegoMinifigure.new("Jim")
     @lego_item = LegoItem.new("Wand", 5)
+    @lego_item_heavy = LegoItem.new("Sword", 11)
+    @threshold = 10
+  end
+
+  def test_to_string_on_hat
+    expected = "a medium blue ball cap"
+    assert_equal(expected, @lego_hat.to_s)
+  end
+
+  def test_to_string_on_item
+    expected = "a 5 gram Wand"
+    assert_equal(expected, @lego_item.to_s)
+  end
+
+  def test_is_heavy_on_item
+    assert(@lego_item.is_heavy(@threshold) == false)
+  end
+
+  def test_is_heavy_on_heavy_item
+    assert(@lego_item_heavy.is_heavy(@threshold))
   end
 
   def test_to_string_on_figure_without_any_items
@@ -14,9 +34,22 @@ class TestLego < Minitest::Test
     assert_equal(expected, @lego_jim.to_s)
   end
 
-  def test_to_string_on_hat
-    expected = "a medium blue ball cap"
-    assert_equal(expected, @lego_hat.to_s)
+  def test_to_string_on_figure_with_hat
+    @lego_jim.wear_hat(@lego_hat.to_s)
+    expected = "A Lego minifigure named Jim, who is wearing a medium blue ball cap."
+    assert_equal(expected, @lego_jim.to_s)
+  end
+
+  def test_to_string_on_figure_with_only_left_item
+    @lego_jim.place_in_left_hand(@lego_item.to_s)
+    expected = "A Lego minifigure named Jim, who is holding a 5 gram Wand in the left hand."
+    assert_equal(expected, @lego_jim.to_s)
+  end
+
+  def test_to_string_on_figure_with_only_right_item
+    @lego_jim.place_in_right_hand(@lego_item.to_s)
+    expected = "A Lego minifigure named Jim, who is holding a 5 gram Wand in the right hand."
+    assert_equal(expected, @lego_jim.to_s)
   end
 
   def test_is_strong_when_figure_has_no_items
